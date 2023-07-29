4728b15b1f3a3ae554cee282b3e350d332ffe57c
Roberto Carvalho <robertodecarvalho0608@gmail.com>
2023-07-29 20:34:43 -0300

    exercicio 1 completo, porém ainda com dúvidas

---
+++ index.js
@@ -0,0 +1,15 @@
+function trocarCorDoQuadro () {
+    let trocarCor = document.getElementsByClassName('superior');
+
+    let corAlterada = trocarCor[0];
+    corAlterada.style.backgroundColor = 'yellow';
+}
+
+// let trocarCorDoQuadro = document.getElementsByClassName('trocar-cor')
+
+// function trocarCorDoQuadro () {
+//     let trocarCor = document.getElementsByClassName('superior');
+
+//     let corAlterada = trocarCor[0];
+//     corAlterada.style.backgroundColor = 'yellow'
+// }
