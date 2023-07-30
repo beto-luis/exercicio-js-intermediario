5abc909361bf565b2beaa30b771195e150a29f9b
Roberto Carvalho <robertodecarvalho0608@gmail.com>
2023-07-30 14:16:41 -0300

    Correção do curso

--- exercicio4/exercicio4.js
+++ exercicio4/exercicio4.js
@@ -1,4 +1,6 @@
-const inputVazio = document.getElementById('inputvazio')
+// Minha resolução - só funciona utilizando o id do HTML
+/*
+const inputVazio = document.getElementById('input-vazio')
 
 inputVazio.addEventListener('input', () => {
     if (inputVazio.value.trim() === '') {
@@ -7,3 +9,19 @@ inputVazio.addEventListener('input', () => {
         inputVazio.classList.add('borda-verde')
     }
 })
+
+*/
+
+// Correção do curso
+
+const inputVazio = document.querySelectorAll(".input-vazio");
+
+inputVazio.forEach(input => {
+    input.addEventListener('change', () => {
+        if (input.value !== "") {
+            input.classList.add("borda-verde");
+        } else {
+            input.classList.remove("borda-verde")
+        }
+    });
+});
