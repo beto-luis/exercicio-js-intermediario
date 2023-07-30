227700cf34ee3ab58187f4178861a422280f7779
Roberto Carvalho <robertodecarvalho0608@gmail.com>
2023-07-29 21:51:53 -0300

    Pausa pra descanso, mais de 6 horas de estudos no dia de hoje

---
+++ exercicio3/exercicio3.js
@@ -0,0 +1,6 @@
+const verificacaoDeCor = document.getElementsByClassName('quadro');
+console.log(verificacaoDeCor)
+
+const possuiCorAzul = verificacaoDeCor.classList.contains("azul");
+
+console.log(possuiCorAzul)
