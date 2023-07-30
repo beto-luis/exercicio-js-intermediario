8f520ae13fe2301fcf13c1f0f3ea44cb043eb113
Roberto Carvalho <robertodecarvalho0608@gmail.com>
2023-07-29 21:01:35 -0300

    Tentativa de correção falhou, mantive minha tentativa por hora

--- exercicio1.js
+++ exercicio1.js
@@ -1,15 +1,25 @@
+//Minha tentativa!
+
 function trocarCorDoQuadro () {
-    let trocarCor = document.getElementsByClassName('superior');
+    let trocarCor = document.getElementsByClassName('azul');
 
     let corAlterada = trocarCor[0];
     corAlterada.style.backgroundColor = 'yellow';
 }
 
-// let trocarCorDoQuadro = document.getElementsByClassName('trocar-cor')
 
-// function trocarCorDoQuadro () {
-//     let trocarCor = document.getElementsByClassName('superior');
 
-//     let corAlterada = trocarCor[0];
-//     corAlterada.style.backgroundColor = 'yellow'
-// }
+// Correção!
+/*
+const btnTrocarCor = document.getElementById("trocar-cor");
+
+const quadroAzul = document.querySelector("azul");
+
+btnTrocarCor.addEventListener("click", trocarCor);
+
+function trocarCor() {
+    quadroAzul.classList.remove("azul");
+    quadroAzul.classList.add("amarelo");
+}
+
+*/
